b1ebc5989e18c36098c16c71cb943b0a09008582
brianpaperless <brian@thepaperlessagent.com>
2021-11-06 10:14:59 -0500

    Begin styling Chord Calculator

--- src/components/pages/ChordCalculator.js
+++ src/components/pages/ChordCalculator.js
@@ -1,32 +1,33 @@
-import {useState} from 'react';
-import Score from '../Score';
-import RootSelect from '../forms/RootSelect';
-import ClefSelect from '../forms/ClefSelect';
-import AccidentalSelect from '../forms/AccidentalSelect';
-import {chordQualities, sevenths} from '../../utils/musicConstants'
-import {calculateChord} from '../../utils/musicFunctions';
-import * as Tone from 'tone';
+import { useState } from "react";
+import Score from "../Score";
+import RootSelect from "../forms/RootSelect";
+import ClefSelect from "../forms/ClefSelect";
+import AccidentalSelect from "../forms/AccidentalSelect";
+import { chordQualities, sevenths } from "../../utils/musicConstants";
+import { calculateChord } from "../../utils/musicFunctions";
+import * as Tone from "tone";
 
-import { Button } from '@material-ui/core';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import MenuItem from '@material-ui/core/MenuItem';
-
+import { Grid, useMediaQuery } from "@material-ui/core";
+import Buttons from "../forms/Buttons";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import MenuItem from "@material-ui/core/MenuItem";
 
 export default function ChordCalculator() {
   const [notes, setNotes] = useState({
-    vexStr: '(C4 E4 G4)/w',
-    display: 'C E G',
-    clef: 'treble',
-    toneArr: ['C4', 'E4', 'G4'],
+    vexStr: "(C4 E4 G4)/w",
+    display: "C E G",
+    clef: "treble",
+    toneArr: ["C4", "E4", "G4"],
   });
-  const [sound, setSound] = useState('off');
+  const [sound, setSound] = useState("off");
   const [inversion, setInversion] = useState(0);
-  const [clef, setClef] = useState('treble');
-  const [root, setRoot] = useState('C');
-  const [quality, setQuality] = useState('major');
-  const [accidental, setAccidental] = useState('natural');
+  const [clef, setClef] = useState("treble");
+  const [root, setRoot] = useState("C");
+  const [quality, setQuality] = useState("major");
+  const [accidental, setAccidental] = useState("natural");
+  const isMobile = useMediaQuery("(max-width:599px)");
 
   const handleAccidentalChange = (e) => setAccidental(e.target.value);
   const handleClefChange = (e) => setClef(e.target.value);
@@ -36,62 +37,97 @@ export default function ChordCalculator() {
   const handleQualityChange = (e) => setQuality(e.target.value);
 
   const handleClick = () => {
-    const chord = calculateChord(root, accidental, quality, clef, inversion)
+    const chord = calculateChord(root, accidental, quality, clef, inversion);
     setNotes(chord);
-    if (sound !== 'off') {
+    if (sound !== "off") {
       let timeStart = 0;
       let timeAdd = null;
-      (sound === 'arpeggio') ? timeAdd = 0.5 : timeAdd = 0;
+      sound === "arpeggio" ? (timeAdd = 0.5) : (timeAdd = 0);
       const synth = new Tone.PolySynth(Tone.Synth).toDestination();
-      const now = Tone.now()
-      chord.toneArr.forEach(note => {
-        synth.triggerAttackRelease(note, '2n', now + timeStart);
+      const now = Tone.now();
+      chord.toneArr.forEach((note) => {
+        synth.triggerAttackRelease(note, "2n", now + timeStart);
         timeStart += timeAdd;
       });
-    }  
-  }
+    }
+  };
 
-  const styles = {width: '33%'};
-
-    return (
-      <div className="content">
+  return (
+    <>
+      <Grid item xs={12}>
         <h2>Chord Calculator</h2>
-        <div style={{maxWidth: '600px', margin: 'auto'}}>
-        <FormControl style={styles} >
-          <ClefSelect value={clef} onChange={handleClefChange} />
-        </FormControl>
-        <FormControl style={styles} >
-          <RootSelect value={root} onChange={handleRootChange}/>
-        </FormControl>
-        <FormControl style={styles} >
-          <AccidentalSelect value={accidental} onChange={handleAccidentalChange} />
-        </FormControl>
-        <FormControl style={styles} >
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <ClefSelect value={clef} onChange={handleClefChange} />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <RootSelect value={root} onChange={handleRootChange} />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <AccidentalSelect
+          value={accidental}
+          onChange={handleAccidentalChange}
+        />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <FormControl fullWidth>
           <InputLabel id="quality-label">Quality</InputLabel>
-            <Select id="quality" labelId="quality-label" value={quality} onChange={handleQualityChange}>
-              {chordQualities.map(quality => <MenuItem key={quality} value={quality}>{quality}</MenuItem>)}
-            </Select>
+          <Select
+            id="quality"
+            labelId="quality-label"
+            value={quality}
+            onChange={handleQualityChange}
+          >
+            {chordQualities.map((quality) => (
+              <MenuItem key={quality} value={quality}>
+                {quality}
+              </MenuItem>
+            ))}
+          </Select>
         </FormControl>
-        <FormControl style={styles} >
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <FormControl fullWidth>
           <InputLabel id="inverion-label">Inversion</InputLabel>
-          <Select id="inversion" labelId="inversion-label" value={inversion} onChange={handleInversionChange}>
+          <Select
+            id="inversion"
+            labelId="inversion-label"
+            value={inversion}
+            onChange={handleInversionChange}
+          >
             <MenuItem value={0}>root position</MenuItem>
             <MenuItem value={1}>1st inversion</MenuItem>
             <MenuItem value={2}>2nd inversion</MenuItem>
-            {sevenths.includes(quality) ? <MenuItem value={3}>3rd inversion</MenuItem> : null}
+            {sevenths.includes(quality) ? (
+              <MenuItem value={3}>3rd inversion</MenuItem>
+            ) : null}
           </Select>
         </FormControl>
-        <FormControl style={styles} >
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <FormControl fullWidth>
           <InputLabel id="sound-label">Sound</InputLabel>
-          <Select id="sound" labelId="sound-label" value={sound} onChange={handleSoundChange}>
+          <Select
+            id="sound"
+            labelId="sound-label"
+            value={sound}
+            onChange={handleSoundChange}
+          >
             <MenuItem value="off">off</MenuItem>
             <MenuItem value="chord">chord</MenuItem>
             <MenuItem value="arpeggio">arpeggio</MenuItem>
           </Select>
         </FormControl>
-        </div>
-        <Button variant='contained' color='primary' onClick={handleClick}>Calculate</Button>
-        <Score notes={notes} numNotes="1"/>
-      </div>
-    );
-  }
+      </Grid>
+
+      <Buttons 
+        onCalculate={handleClick}
+        onPlay={() => null}
+        onClear={() => null}
+        showStaff={false}
+        isMobile={isMobile}
+      />
+      <Score notes={notes} numNotes="1" />
+    </>
+  );
+}
--- src/components/pages/NoteID.js
+++ src/components/pages/NoteID.js
@@ -1,142 +1,169 @@
-import { alphabet } from '../../utils/musicConstants';
-import { Button } from '@material-ui/core';
-import { useEffect, useState } from 'react';
-import Score from '../Score';
-import ClefSelect from '../forms/ClefSelect';
-import TextField from '@material-ui/core/TextField';
-import FormControl from '@material-ui/core/FormControl';
+import { alphabet } from "../../utils/musicConstants";
+import { Button, Box, Grid } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import Score from "../Score";
+import ClefSelect from "../forms/ClefSelect";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
 
 const scoreStyles = {
-    display: 'flex',
-    gap: '20px',
-    justifyContent: 'center'
-}
+  display: "flex",
+  gap: "20px",
+  justifyContent: "center",
+};
 export default function NoteID(props) {
+  const [numAttempts, setNumAttempts] = useState(0);
+  const [octaves, setOctaves] = useState(["4", "5"]);
+  const [name, setName] = useState("");
+  const [notes, setNotes] = useState({
+    vexStr: null,
+    display: "",
+    clef: "treble",
+    octave: "4",
+  });
+  const [numCorrect, setNumCorrect] = useState(0);
+  const [currentNote, setCurrentNote] = useState(
+    alphabet[Math.floor(Math.random() * (alphabet.length - 1))]
+  );
+  const [PCT, setPCT] = useState(0);
+  const [emoji, setEmoji] = useState(null);
 
-    const [numAttempts, setNumAttempts] = useState(0);
-    const [octaves, setOctaves] = useState(['4', '5']);
-    const [name, setName] = useState('');
-    const [notes, setNotes] = useState({
-        vexStr: null,
-        display: '',
-        clef: 'treble',
-        octave: '4',
-    })
-    const [numCorrect, setNumCorrect] = useState(0);
-    const [currentNote, setCurrentNote] = useState(alphabet[Math.floor(Math.random() * (alphabet.length - 1))]);
-    const [PCT, setPCT] = useState(0);
-    const [emoji, setEmoji] = useState(null);
-
-    const changeClef = (e) => {
-        let range = [4, 5];
-        if (e.target.value === 'alto') {
-            range = [3, 4];
-        } else if (e.target.value === 'bass') {
-            range = [2, 3]
-        }
-        setOctaves(range);
-        setNotes({
-            vexStr: currentNote + '4/w',
-            display: '',
-            clef: e.target.value,
-            octave: range[Math.floor(Math.random() * 2)],
-        })
+  const changeClef = (e) => {
+    let range = [4, 5];
+    if (e.target.value === "alto") {
+      range = [3, 4];
+    } else if (e.target.value === "bass") {
+      range = [2, 3];
     }
+    setOctaves(range);
+    setNotes({
+      vexStr: currentNote + "4/w",
+      display: "",
+      clef: e.target.value,
+      octave: range[Math.floor(Math.random() * 2)],
+    });
+  };
 
-    const onNameChange = (e) => {
-        setName(e.target.value);
+  const onNameChange = (e) => {
+    setName(e.target.value);
+  };
+  const makeGuess = (e) => {
+    let updatedAttempts = numAttempts + 1;
+    let pct = null;
+    setNumAttempts(updatedAttempts);
+    const newGuess = e.target.innerText;
+    if (newGuess === currentNote) {
+      const newNote =
+        alphabet[Math.floor(Math.random() * (alphabet.length - 1))];
+      const newOctave = octaves[Math.floor(Math.random() * 2)];
+      const updatedCorrect = numCorrect + 1;
+      setNumCorrect(updatedCorrect);
+      setCurrentNote(newNote);
+      setNotes({
+        ...notes,
+        vexStr: newNote + newOctave + "/w",
+        display: "",
+        octave: newOctave,
+      });
+      pct = Math.round((updatedCorrect / updatedAttempts) * 100);
+      setPCT(pct);
+    } else {
+      pct = Math.round((numCorrect / updatedAttempts) * 100);
+      setPCT(pct);
     }
-    const makeGuess = (e) => {
-        let updatedAttempts = numAttempts + 1;
-        let pct = null;
-        setNumAttempts(updatedAttempts);
-        const newGuess = e.target.innerText;
-        if (newGuess === currentNote) {
-            const newNote = alphabet[Math.floor(Math.random() * (alphabet.length - 1))];
-            const newOctave = octaves[Math.floor(Math.random() * 2)];
-            const updatedCorrect = numCorrect + 1;
-            setNumCorrect(updatedCorrect);
-            setCurrentNote(newNote);
-            setNotes({...notes,
-                vexStr: newNote + newOctave + '/w',
-                display: '',
-                octave: newOctave,
-            })
-            pct = Math.round((updatedCorrect / updatedAttempts) * 100);
-            setPCT(pct);
-        } else {
-            pct = Math.round((numCorrect / updatedAttempts) * 100);
-            setPCT(pct);
-        } 
-        if (pct > 90) setEmoji('😁');
-        else if (pct > 80) setEmoji('😀');
-        else if (pct > 70) setEmoji('😐');
-        else if (pct > 60) setEmoji('😟');
-        else setEmoji('😭');
-    }
+    if (pct > 90) setEmoji("😁");
+    else if (pct > 80) setEmoji("😀");
+    else if (pct > 70) setEmoji("😐");
+    else if (pct > 60) setEmoji("😟");
+    else setEmoji("😭");
+  };
 
-    const submitScore = () => {
-        const score = {
-            name: name,
-            numCorrect: numCorrect,
-            numAttempts: numAttempts,
-            pct: Math.round((numCorrect / numAttempts) * 100),
-            game: 'Note ID',
-        }
+  const submitScore = () => {
+    const score = {
+      name: name,
+      numCorrect: numCorrect,
+      numAttempts: numAttempts,
+      pct: Math.round((numCorrect / numAttempts) * 100),
+      game: "Note ID",
+    };
 
-
-        fetch('https://sheet.best/api/sheets/fa8cefd4-9b56-41da-8404-f1bb6419f02c', {
-            method: 'POST',
-            headers: {
-                'X-API-KEY': 'Rj-G_Fdl-q#fRaEaaC299_ThGg-0zS4HcDWQ4ky3w5J1MmeJ6t-U5rY0adfa8qnG',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(score)
-        })
-        .then(response => response.json())
-        .then(response => {
-            console.log(response);
-            props.setRoute('Leaderboard');
-        })
-        .catch(error => console.log(error));
-
-
-        setName('');
-        setNumAttempts(0);
-        setNumCorrect(0);
-        setEmoji(null);
-        setPCT(0);
-
-    }
-    useEffect(() => {
-        const octave = octaves[Math.floor(Math.random() * 2)];
-        setNotes({...notes,
-            vexStr: currentNote + octave + '/w',
-            display: '',
-            octave: octave
-        }) // eslint-disable-next-line
-    }, [])
-    
-    return (
-        <div className='content'>
-            <h2>Note Identification</h2>
-            <h3 style={scoreStyles}>
-                <span>Score:</span>
-                <span>{numCorrect} / {numAttempts}</span>
-                <span>{PCT}%</span>
-                <span>{emoji}</span>
-            </h3>
-            <FormControl style={{width: '120px'}}>
-                <ClefSelect onChange={changeClef} />
-            </FormControl>
-            {(notes.vexStr !== null) ? <Score notes={notes} numNotes={1} /> : null}
-            <div>
-                {alphabet.map(note => <Button variant='contained' onClick={makeGuess} color='primary'>{note}</Button>)}
-            </div>
-            <div> 
-                <TextField value={name} onChange={onNameChange} id="standard-basic" label="Name" />
-                <Button onClick={submitScore} variant="contained" color="primary">Submit Score</Button>
-            </div>
-        </div>
+    fetch(
+      "https://sheet.best/api/sheets/fa8cefd4-9b56-41da-8404-f1bb6419f02c",
+      {
+        method: "POST",
+        headers: {
+          "X-API-KEY":
+            "Rj-G_Fdl-q#fRaEaaC299_ThGg-0zS4HcDWQ4ky3w5J1MmeJ6t-U5rY0adfa8qnG",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(score),
+      }
     )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        props.setRoute("Leaderboard");
+      })
+      .catch((error) => console.log(error));
+
+    setName("");
+    setNumAttempts(0);
+    setNumCorrect(0);
+    setEmoji(null);
+    setPCT(0);
+  };
+  useEffect(() => {
+    const octave = octaves[Math.floor(Math.random() * 2)];
+    setNotes({
+      ...notes,
+      vexStr: currentNote + octave + "/w",
+      display: "",
+      octave: octave,
+    }); // eslint-disable-next-line
+  }, []);
+
+  return (
+    <>
+      <Grid item xs={12}>
+        <h2>Note Identification</h2>
+      </Grid>
+      <Grid item xs={12}>
+        <h3 style={scoreStyles}>
+          <span>Score:</span>
+          <span>
+            {numCorrect} / {numAttempts}
+          </span>
+          <span>{PCT}%</span>
+          <span>{emoji}</span>
+        </h3>
+      </Grid>
+      <Grid xs={12} item>
+        {notes.vexStr !== null ? <Score notes={notes} numNotes={1} /> : null}
+      </Grid>
+      <Grid item xs={12}>
+        <ClefSelect onChange={changeClef} />
+      </Grid>
+      <Grid item xs={12}>
+        <Box style={{ display: "flex", justifyContent: "space-around" }}>
+          {alphabet.map((note) => (
+            <Button fullWidth variant="contained" onClick={makeGuess}>
+              {note}
+            </Button>
+          ))}
+        </Box>
+      </Grid>
+      <div>
+        <TextField
+          value={name}
+          onChange={onNameChange}
+          id="standard-basic"
+          label="Name"
+        />
+
+        <Button onClick={submitScore} variant="contained" color="primary">
+          Submit Score
+        </Button>
+      </div>
+    </>
+  );
 }
